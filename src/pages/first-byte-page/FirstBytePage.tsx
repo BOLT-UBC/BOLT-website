22ea44ef1fc8e2940070ceb9cb5810cebc2cc9ac
Davooood90 <davooood90@gmail.com>
2024-10-16 19:36:22 -0700

    Created 8-bit button component

--- src/pages/first-byte-page/FirstBytePage.tsx
+++ src/pages/first-byte-page/FirstBytePage.tsx
@@ -3,6 +3,7 @@ import "./styles.css";
 import Background from "./components/Background";
 import Banner from "./components/Banner";
 import Folder from "./components/Folder";
+import Button from "./components/Button";
 
 //fb stands for first-byte
 const FirstBytePage: FC = () => {
@@ -16,6 +17,7 @@ const FirstBytePage: FC = () => {
           sponsorLogo="https://media.discordapp.net/attachments/859197594831552542/1296256765914120192/image.png?ex=6711a0b0&is=67104f30&hm=756467d14d2fbabd2494a0f4cfa8769cad9f67e8db7dea38c71c62976bed9991&=&format=webp&quality=lossless&width=712&height=471"
         ></Folder>
       </div>
+      <Button buttonText="Apply Now" buttonRedir="#about" />
     </div>
   );
 };
