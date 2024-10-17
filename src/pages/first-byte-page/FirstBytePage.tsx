5d392937b4d8d6f360d3b5ab7d454a0b5e5fc762
Davooood90 <davooood90@gmail.com>
2024-10-16 18:41:19 -0700

    Sponsor component icon

--- src/pages/first-byte-page/FirstBytePage.tsx
+++ src/pages/first-byte-page/FirstBytePage.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import "./styles.css";
 import Background from "./components/Background";
 import Banner from "./components/Banner";
+import Folder from "./components/Folder";
 
 //fb stands for first-byte
 const FirstBytePage: FC = () => {
@@ -9,6 +10,12 @@ const FirstBytePage: FC = () => {
     <div className="fb-body">
       <Banner />
       <Background />
+      <div className="fb-sponsors">
+        <Folder
+          sponsorName="mastercard"
+          sponsorLogo="https://media.discordapp.net/attachments/859197594831552542/1296256765914120192/image.png?ex=6711a0b0&is=67104f30&hm=756467d14d2fbabd2494a0f4cfa8769cad9f67e8db7dea38c71c62976bed9991&=&format=webp&quality=lossless&width=712&height=471"
+        ></Folder>
+      </div>
     </div>
   );
 };
