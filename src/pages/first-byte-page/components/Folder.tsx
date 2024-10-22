d39805e1349937e3340117f658c63bb85db50b6b
Davooood90 <davooood90@gmail.com>
2024-10-22 16:06:47 -0700

    Update folder to use svg and be scaleable

--- src/pages/first-byte-page/components/Folder.tsx
+++ src/pages/first-byte-page/components/Folder.tsx
@@ -2,29 +2,23 @@ import { FC } from "react";
 import "./Folder.css";
 
 interface FolderProps {
-  sponsorName: string;
-  sponsorLogo: string;
+  folderName: string;
+  folderLogo: string;
+  folderLink: string;
 }
 
-const Folder: FC<FolderProps> = ({ sponsorName, sponsorLogo }) => {
+const Folder: FC<FolderProps> = ({ folderName, folderLogo, folderLink }) => {
   return (
     <>
-      <div className="sponsor-display">
-        <div className="fb-folder-shadow">
-          <div className="folder-tab-outline-shadow"></div>
-          <div className="folder-tab-shadow"></div>
-          <div className="folder-body-shadow"></div>
-        </div>
-        <div className="fb-folder">
-          <div className="folder-tab-outline"></div>
-          <div className="folder-tab"></div>
-          <div className="folder-tab-line"></div>
-          <div className="folder-body">
-            <img className="sponsor-logo" src={sponsorLogo} />
-            <h1 className="sponsor-label">{sponsorName}</h1>
-          </div>
-        </div>
-      </div>
+      <a className="sponsor-display" href={folderLink}>
+        <img className="sponsor-logo" src={folderLogo} />
+        <h1 className="sponsor-label folder-text">{folderName}</h1>
+        <img
+          className="sponsor-folder"
+          src="/assets/first-byte/fb-folder.svg"
+          alt="My Happy SVG"
+        />
+      </a>
     </>
   );
 };
