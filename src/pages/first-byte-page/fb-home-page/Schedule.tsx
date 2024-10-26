8677aeb691c1b59f4b956f0626c820741d7e3ab3
Davooood90 <davooood90@gmail.com>
2024-10-25 20:48:08 -0700

    fix schedule descriptions

--- src/pages/first-byte-page/fb-home-page/Schedule.tsx
+++ src/pages/first-byte-page/fb-home-page/Schedule.tsx
@@ -9,37 +9,37 @@ const Schedule: FC = () => {
         <Task
           TaskName="Registration"
           TimeName="12:45 PM"
-          DescName="Yappity yap yap time to check in at the address (dwadwadwa)"
+          DescName="Check in, grab your stuff, and get ready to roll"
         />
         <Task
           TaskName="Openning"
           TimeName="1:00 PM"
-          DescName="Lunch Time! We will be providing food!!! Yum yum in my tum tum AY PAYAPYAYPYPAPYAYPAPYAYPAYPAYPAPYAYPAYPA PYAYPAPYAPY A YPYAYPYPAY PAYPAYAPA YPYPAPY"
+          DescName="A warm welcome to get everyone pumped for the day!"
         />
         <Task
           TaskName="Breakout Room Presentations"
           TimeName="1:15 PM"
-          DescName="Yappity yap yap time to leave bai bai hope you had fun"
+          DescName="Small groups share ideas—let’s see what everyone’s working on!"
         />
         <Task
           TaskName="Breaks & Snacks"
           TimeName="3:15 PM"
-          DescName="Yappity yap yap time to leave bai bai hope you had fun"
+          DescName="Take a breather, grab a snack, and mingle."
         />
         <Task
           TaskName="Finalist Presentations"
           TimeName="4:15 PM"
-          DescName="Yappity yap yap time to leave bai bai hope you had fun"
+          DescName="Our star projects hit the spotlight—don’t miss it!"
         />
         <Task
           TaskName="Awards & Closing Remarks"
           TimeName="5:15 PM"
-          DescName="Yappity yap yap time to leave bai bai hope you had fun"
+          DescName="Celebrating wins and wrapping things up."
         />
         <Task
           TaskName="Networking"
           TimeName="5:45 PM"
-          DescName="Yappity yap yap time to leave bai bai hope you had fun"
+          DescName="Wrap up with some good chats and new connections."
         />
       </div>
     </>
