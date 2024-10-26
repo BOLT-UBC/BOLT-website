7b7da243ed2df1200d0c8e553b36e4d836e9df98
tyu13 <tyu13@student.ubc.ca>
2024-10-26 15:57:01 -0700

    set first byte page as home directory, removed other pages for now

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import Home from "./pages/home/Home.tsx";
 import "./app.css"
 import AboutPage from "./pages/about-page/AboutPage.tsx";
 import EventsPage from "./pages/events-page/EventsPage.tsx";
+import FirstBytePage from './pages/first-byte-page/FirstBytePage.tsx';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<FirstBytePage />} />
+                {/* <Route path="/" element={<Home />} />
                 <Route path="/aboutus" element={<AboutPage />} />
-                <Route path="/events" element={<EventsPage />} />
+                <Route path="/events" element={<EventsPage />} /> */}
             </Routes>
         </Router>
     )
