4ca52a1a820a9dcceace982f6687545ac89424cb
tyu13 <tyu13@student.ubc.ca>
2024-10-25 17:05:00 -0700

    pulled in first byte page changes from main and adapted images to nextjs format, set the root directory as the first byte page

--- app/[[...slug]]/client.tsx
+++ app/[[...slug]]/client.tsx
@@ -8,6 +8,7 @@ import dynamic from 'next/dynamic'
 const Home = dynamic(() => import('../../src/pages/home/Home'), { ssr: false });
 const About = dynamic(() => import('../../src/pages/about-page/AboutPage'), { ssr: false });
 const Events = dynamic(() => import('../../src/pages/events-page/EventsPage'), { ssr: false });
+const FirstBytePage = dynamic(() => import('../../src/pages/first-byte-page/FirstBytePage'), { ssr: false });
 
 export function ClientOnly({ page }: { page: string }) {
   if (page === 'home') {
@@ -20,6 +21,10 @@ export function ClientOnly({ page }: { page: string }) {
 
   if (page === 'events') {
     return <Events />;
+  }
+
+  if (page === '') {
+    return <FirstBytePage />;
   }
 
   return <h1>404: Page Not Found</h1>;
