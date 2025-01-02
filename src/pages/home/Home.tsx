5cf4602b256f8d5e76ad31bb359eb4b2fbd8a610
tyu13 <tyu13@student.ubc.ca>
2025-01-02 15:55:26 -0800

    temporarily got rid of home page components, pput button back

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,12 @@
 import { motion, useTransform, useViewportScroll } from 'framer-motion';
 import BoltUBCTitle from './components/BoltUBCTitle.tsx';
-import Sponsors from './components/Sponsors.tsx';
+// import Sponsors from './components/Sponsors.tsx';
+// import Events from "./components/Events.tsx";
+// import OurMission from "./components/OurMission.tsx";
 
 import './style.css';
 
 import {FC} from "react";
-import BoltUBCTitleMobile from "./components/BoltUBCTitleMobile.tsx";
-import SponsorsMobile from "./components/SponsorsMobile.tsx";
-import Events from "./components/Events.tsx";
-import EventsMobile from "./components/EventsMobile.tsx";
-import OurMissionMobile from "./components/OurMissionMobile.tsx";
-import OurMission from "./components/OurMission.tsx";
 import Navbar from "../../global-components/Navbar.tsx";
 
 const Home: FC = () => {
@@ -18,44 +14,6 @@ const Home: FC = () => {
 
     const fasterScroll = useTransform(scrollY, [0, 1000], [0, -100]);
 
-    const details: string = navigator.userAgent;
-
-    const regexp: RegExp = /android|iphone|kindle|ipad/i;
-
-    const isMobileDevice: boolean = regexp.test(details);
-
-    if (isMobileDevice) {
-        return (
-            <body style={{ width: '100%', overflowX: 'hidden' }}>
-            <div className="area">
-                <motion.ul
-                    className="circles"
-                    style={{ y: fasterScroll }}
-                >
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </motion.ul>
-            </div>
-            <div className="context">
-                <Navbar />
-                <BoltUBCTitleMobile />
-                <SponsorsMobile />
-                <OurMissionMobile />
-                <div style={{ height: '50px' }}></div>
-                <EventsMobile />
-                <div style={{ height: '100px' }}></div>
-            </div>
-            </body>
-        );
-    } else {
         return (
             <body>
             <div className="area">
@@ -78,14 +36,14 @@ const Home: FC = () => {
             <div className="context">
                 <Navbar />
                 <BoltUBCTitle />
-                <Sponsors />
-                <OurMission />
+                {/* <Sponsors />
+                <OurMission /> */}
+                {/* <Events /> */}
                 <div style={{ height: '100px' }}></div>
-                <Events />
             </div>
             </body>
         );
     }
-}
+
 
 export default Home;
