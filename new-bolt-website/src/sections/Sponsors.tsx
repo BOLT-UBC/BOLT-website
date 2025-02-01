2623ae7cab596c5ca62b7f647526cc0737c39cee
angelafeliciaa <angela_felicia@yahoo.com>
2025-01-31 19:29:42 -0800

    sponsors in progress

--- new-bolt-website/src/sections/Sponsors.tsx
+++ new-bolt-website/src/sections/Sponsors.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './Sponsors.module.css';
+import sponsorsImage from "../assets/images/Sponsors.png";
 
 const Sponsors: React.FC = () => {
-  return <div className={styles.Sponsors}>These ppl gave us money</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <img src={sponsorsImage} alt="Sponsors" className="w-24 h-auto" />
+    </div>
+  );
 };
 
 export default Sponsors;
