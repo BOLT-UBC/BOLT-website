060bec906a3ccbd2fbfff835385f0dcdbc3f99f8
Davooood90 <davooood90@gmail.com>
2025-02-12 13:54:03 -0800

    under construction

---
+++ bootcamp/frontend/src/pages/UnderConstruction.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./UnderConstruction.css";
+
+export default function InProgress() {
+  return (
+    <div className="ip-background">
+      <div className="ip-content">
+        <h1>Hmm... You caught us under construction!</h1>
+      </div>
+    </div>
+  );
+}
