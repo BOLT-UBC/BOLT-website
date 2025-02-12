3577ba110d5a44db7a55c0dfcb308ee278694daa
Davooood90 <davooood90@gmail.com>
2025-02-12 12:55:50 -0800

    reset

--- bootcamp/frontend/src/pages/sections/Title.tsx
+++ bootcamp/frontend/src/pages/sections/Title.tsx
@@ -12,7 +12,7 @@ export default function Title() {
       <div className="title-background">
         <Rocket targetDate="2025-02-24T23:59:59" />
         <div className="title-content">
-          <h1>Bootcamp!</h1>
+          <h1>Bootcamp</h1>
           <h2>March 9-10, 2025 | BOLT UBC</h2>
           <h3>Register by Monday, February 24, 2025!</h3>
           <div className="button-container">
