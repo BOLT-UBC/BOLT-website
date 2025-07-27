546e36657c95754adfe99cbe4cf13025e0edc8c4
Davooood90 <davooood90@gmail.com>
2025-07-26 20:16:16 -0700

    replaced background image gradient with css gradient

--- new-bolt-website/src/Home.tsx
+++ new-bolt-website/src/Home.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Title from './sections/Title';
-import About from './sections/About.tsx';
-import Events from './sections/Events.tsx';
-import Sponsors from './sections/Sponsors.tsx';
-import Team from './sections/Team.tsx';
-import Navbar from './components/Navbar.tsx';
-import styles from './Home.module.css';
+import React from "react";
+import Title from "./sections/Title";
+import About from "./sections/About.tsx";
+import Events from "./sections/Events.tsx";
+import Sponsors from "./sections/Sponsors.tsx";
+import Team from "./sections/Team.tsx";
+import Navbar from "./components/Navbar.tsx";
+import styles from "./Home.module.css";
 
 const Home: React.FC = () => {
   return (
