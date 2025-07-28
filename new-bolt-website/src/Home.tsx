fa6426e5ccc6d03fb26cae7e7987e837f62e69a4
Davooood90 <davooood90@gmail.com>
2025-07-27 20:55:45 -0700

    replaced image gradient for css gradient

--- new-bolt-website/src/Home.tsx
+++ new-bolt-website/src/Home.tsx
@@ -14,8 +14,9 @@ const Home: React.FC = () => {
         <Navbar />
         <Title />
         <About />
-        <Sponsors />
       </div>
+      <Sponsors />
+
       <Events />
       <Team />
     </>
--- new-bolt-website/src/sections/Sponsors.tsx
+++ new-bolt-website/src/sections/Sponsors.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import styles from './Sponsors.module.css';
+import React from "react";
+import styles from "./Sponsors.module.css";
 import sponsorsImage from "../assets/images/Sponsors.png";
 
 const Sponsors: React.FC = () => {
   return (
     <>
       <div className={styles.Sponsors} id="Sponsors">
-        <img 
-          src={sponsorsImage} 
-          alt="Sponsors" 
+        <img
+          src={sponsorsImage}
+          alt="Sponsors"
           className={styles.buildings}
+          loading="lazy"
         />
       </div>
     </>
