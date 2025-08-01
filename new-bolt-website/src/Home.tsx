34ff5a9075eb81cdc0cfaad0c8b6ecad9b0113b7
Davooood90 <davooood90@gmail.com>
2025-08-01 09:57:49 -0700

    implemented responsiveness for title section

--- new-bolt-website/src/Home.tsx
+++ new-bolt-website/src/Home.tsx
@@ -6,23 +6,18 @@ import Sponsors from "./sections/Sponsors.tsx";
 import Team from "./sections/Team.tsx";
 import Navbar from "./components/Navbar.tsx";
 import Footer from "./components/Footer.tsx";
-
 import styles from "./Home.module.css";
 
 const Home: React.FC = () => {
   return (
     <>
-      <div className={styles.container}>
-        <Navbar />
-        <Title />
-        <About />
-      </div>
-      <div className={styles.skyline}>
-        <Sponsors />
-        {/* <Events /> */}
-      </div>
-      <Team />
-      <Footer />
+      {/* <Navbar /> */}
+      <Title />
+      {/* <About />
+      <Sponsors /> */}
+      {/* <Events /> */}
+      {/* <Team />
+      <Footer /> */}
     </>
   );
 };
